c0bca6c8bff0bc7d5b5503aa9cb7d2f74a458673
miyazato-hayato1 <miyazato-hayato1@dmm.com>
2021-05-09 14:13:49 +0900

    初期値変更

--- app/src/main/java/com/mito/stockarticle/ui/tag/add/ArticleTagAddState.kt
+++ app/src/main/java/com/mito/stockarticle/ui/tag/add/ArticleTagAddState.kt
@@ -2,7 +2,7 @@ package com.mito.stockarticle.ui.tag.add
 
 data class ArticleTagAddState(
   val name: String = "",
-  val red: Float = 0f,
-  val green: Float = 0f,
-  val blue: Float = 0f
+  val red: Float = 0.5f,
+  val green: Float = 0.5f,
+  val blue: Float = 0.5f
 )
